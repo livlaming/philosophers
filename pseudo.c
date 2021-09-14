3c69521dc95c84f79516ac32905d1093d6c02b51
Lisa Vlamings <livlamin@f0r1s8.codam.nl>
2021-09-14 17:11:03 +0200

    pseudo

---
+++ pseudo.c
@@ -0,0 +1,16 @@
+
+
+while (philo[ID])
+{
+    lock -> left fork
+    lock-> right fork
+    eat -> aantal sec
+    last_eaten
+    unlock ->left fork
+    unlock -> right fork
+    sleep -> aantal sec
+    think -> tot aan fork
+}
+
+die als te lang niet gegeten
+die als iedereen full is?
