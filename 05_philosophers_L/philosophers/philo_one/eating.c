28f9a49fafbf811dda1c6cec8834cad62d899927
Lisa Vlamings <livlamin@f1r1s9.codam.nl>
2021-10-15 18:55:45 +0200

    update

--- 05_philosophers_L/philosophers/philo_one/eating.c
+++ 05_philosophers_L/philosophers/philo_one/eating.c
@@ -6,7 +6,7 @@
 /*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 14:29:03 by livlamin      #+#    #+#                 */
-/*   Updated: 2021/10/15 18:39:54 by livlamin      ########   odam.nl         */
+/*   Updated: 2021/10/15 15:24:56 by livlamin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,8 @@ void   eating(t_philo *philo)
         pthread_mutex_lock(philo->info->eat);
         write_state("is eating", philo, philo->ID);
         stupid_sleep(philo->info->time_to_eat);
-        // philo->last_eaten = get_time(philo->info->start_time);
-        philo->last_eaten = get_time_seconds();
+        philo->last_eaten = get_time(philo->info->start_time);
         philo->time_left = philo->info->time_to_die;
-        philo->meals_left--;
-        if (philo->meals_left == 0)
-            philo->info->num_of_philo_full++;
         pthread_mutex_unlock(philo->info->eat);
     }
 }
--- 05_philosophers_L/philosophers/philo_one/manage.c
+++ 05_philosophers_L/philosophers/philo_one/manage.c
@@ -6,7 +6,7 @@
 /*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 13:30:14 by livlamin      #+#    #+#                 */
-/*   Updated: 2021/10/15 18:38:34 by livlamin      ########   odam.nl         */
+/*   Updated: 2021/10/15 15:26:30 by livlamin      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@ void    *manage(void *arg)
     philo = arg;
     while(philo->time_left > 0 && philo->info->state == 1)
     {
-        philo->time_left -= get_time_seconds() - philo->last_eaten;
+        philo->time_left -= (get_time(philo->info->start_time) - philo->last_eaten);
         if (philo->time_left <= 0 && philo->info->state == 1)
         {
             philo->info->state = 0;
             philo->time_left = 0;
             pthread_mutex_lock(philo->info->write);
-            printf("%ld %zu died\n", get_time(philo->info->start_time), philo->ID);
+            printf("%d %zu died\n", get_time(philo->info->start_time), philo->ID);
             pthread_mutex_unlock(philo->info->write);
             // write_state("died", philo, philo->ID);
             return ((void*)NULL);
