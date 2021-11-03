0f79cafc92ab70aab44106fdd909cc1800966a55
Lisa Vlamings <livlamin@f1r6s9.codam.nl>
2021-11-03 12:57:21 +0100

    retry

---
+++ TEST_1_retry/philo_one/main.c
@@ -0,0 +1,100 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/26 11:50:51 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/11/03 12:55:50 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+#include <stdlib.h>
+
+static int	check_input(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (argv[i][0] < '0' || argv[i][0] > '9')
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+static int	join_thread(t_central *central, pthread_t *thread)
+{
+	int	i;
+
+	i = 0;
+	while (i < central->num_of_philo)
+	{
+		if (pthread_join(thread[i], NULL) != 0)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+#include <unistd.h>
+
+static int	create_threads(t_central *central, t_philo *philo,
+	int i, pthread_t *thread)
+{
+
+	thread = malloc(sizeof(pthread_t) * central->num_of_philo);
+	while (i < central->num_of_philo)
+	{
+		if (i & 1) //even
+		{
+			if (pthread_create(&thread[i], NULL,
+					routine_even_right_left, &philo[i]) != 0)
+				return (error_message(central, philo, 2));
+		}
+		else //odd
+		{
+			if (pthread_create(&thread[i], NULL,
+					routine_odd_left_right, &philo[i]) != 0)
+				return (error_message(central, philo, 2));
+		}
+		// usleep(1000);
+		i++;
+	}
+	if (join_thread(central, thread) != 0)
+	{
+		free(thread);
+		return (error_message(central, philo, 3));
+	}
+	// unlock_and_destroy(philo, central);
+	free(thread);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_central	*central;
+	t_philo		*philo;
+
+	philo = NULL;
+	central = malloc(sizeof(t_central));
+	if (!central)
+		return (-1);
+	if ((argc != 5 && argc != 6) || check_input(argc, argv) == -1)
+		return (error_message(central, philo, 1));
+	if (init_central_struct(central, argv, argc, 0) == -1)
+		return (error_message(central, philo, 1));
+	philo = malloc(sizeof(t_philo) * central->num_of_philo);
+	if (!philo)
+		return (-1);
+	if (init_philo_struct(central, philo) == -1)
+		return (error_message(central, philo, 1));
+	if (create_threads(central, philo, 0, NULL) == -1)
+		return (-1);
+	// unlock_and_destroy(philo, central);
+	// system("leaks philo_one");
+	return (0);
+}
---
+++ TEST_1_retry/philo_one/routine_even_right_left.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   routine_even_right_left.c                          :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/19 12:58:27 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/11/03 12:48:34 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	*routine_even_right_left(void *arg)
+{
+	t_philo		*philo;
+	pthread_t	director;
+
+	philo = arg;
+	director = NULL;
+	pthread_mutex_lock(philo->central->eat);
+	philo->last_eaten = get_time_mseconds();
+	pthread_mutex_unlock(philo->central->eat);
+	if (pthread_create(&director, NULL, direct, philo) != 0)
+		return ((void *) NULL); // error?
+	while (check_status(philo) == ALIVE)
+	{
+		pthread_mutex_lock(philo->rfork);
+		write_state("has taken a fork", philo, philo->ID);
+		pthread_mutex_lock(philo->lfork);
+		write_state("has taken a fork", philo, philo->ID);
+		eating_right_left(philo);
+		write_state("is sleeping", philo, philo->ID);
+		stupid_sleep(philo->central->time_to_sleep);
+		write_state("is thinking", philo, philo->ID);
+	}
+	if (pthread_join(director, NULL) != 0)
+		return ((void *) NULL); // error?
+	// free(director);
+	return ((void *) NULL);
+}
+
+// 162099 1 is eating
+// 81246 3 is eating
