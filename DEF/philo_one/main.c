d14ad44c2fb19d9f02a21738b17f122433d08eff
Lisa Vlamings <livlamin@f1r6s1.codam.nl>
2021-11-04 15:50:27 +0100

    DEF

---
+++ DEF/philo_one/main.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   main.c                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/26 11:50:51 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/11/04 15:20:56 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+#include <stdlib.h>
+
+static int	check_input(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (argv[i][0] < '0' || argv[i][0] > '9')
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+
+static int	join_thread(t_central *central, pthread_t *thread)
+{
+	int	i;
+
+	i = 0;
+	while (i < central->num_of_philo)
+	{
+		if (pthread_join(thread[i], NULL) != 0)
+			return (-1);
+		i++;
+	}
+	return (0);
+}
+#include <unistd.h>
+static int			when_thread_creation_failed(t_central *central, t_philo *philo, int i, pthread_t *thread)
+{
+	while (i > 0)
+	{
+		--i;
+		if (pthread_join(thread[i], NULL) != 0)
+			return (-1);
+	}
+	return (error_message(central, philo, 2));
+}
+
+static int	create_threads(t_central *central, t_philo *philo,
+	int i, pthread_t *thread)
+{
+
+	thread = malloc(sizeof(pthread_t) * central->num_of_philo);
+	while (i < central->num_of_philo)
+	{
+		if (i & 1) //even
+		{
+			if (pthread_create(&thread[i], NULL,
+					routine_even_right_left, &philo[i]) != 0)
+				return (when_thread_creation_failed(central, philo, i, thread));
+		}
+		else //odd
+		{
+			if (pthread_create(&thread[i], NULL,
+					routine_odd_left_right, &philo[i]) != 0)
+				return (when_thread_creation_failed(central, philo, i, thread));
+		}
+		i++;
+		usleep(1000);
+	}
+	if (join_thread(central, thread) != 0)
+	{
+		free(thread);
+		return (error_message(central, philo, 3));
+	}
+	// unlock_and_destroy(philo, central);
+	// usleep(500);
+	free(thread);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_central	*central;
+	t_philo		*philo;
+
+	philo = NULL;
+	central = malloc(sizeof(t_central));
+	if (!central)
+		return (-1);
+	if ((argc != 5 && argc != 6) || check_input(argc, argv) == -1)
+		return (error_message(central, philo, 1));
+	if (init_central_struct(central, argv, argc, 0) == -1)
+		return (error_message(central, philo, 1));
+	philo = malloc(sizeof(t_philo) * central->num_of_philo);
+	if (!philo)
+		return (-1);
+	if (init_philo_struct(central, philo) == -1)
+		return (error_message(central, philo, 1));
+	if (create_threads(central, philo, 0, NULL) == -1)
+		return (-1);
+	unlock_and_destroy(philo, central);
+	// system("leaks philo_one");
+	return (0);
+}
+
+
+// [philo_one-Test #2]: Given 4 410 200 200 arguments to philo_one, no philosopher should die !
+// [philo_one-Test #3]: Given 4 800 200 200 arguments to philo_one, no philosopher should die !
---
+++ DEF/philo_one/unlock_and_destroy.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   unlock_and_destroy.c                               :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: livlamin <livlamin@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/10/26 11:19:03 by livlamin      #+#    #+#                 */
+/*   Updated: 2021/11/04 15:05:14 by livlamin      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+#include <stdlib.h>
+
+void	destroy_forks(t_philo *philo)
+{
+	int	i;
+
+	i = 0;
+	while (i < philo->central->num_of_forks)
+	{
+		// pthread_mutex_unlock(&philo->central->forks[i]);
+		pthread_mutex_destroy(&philo->central->forks[i]);
+		i++;
+	}
+}
+
+void	destroy_mutex(t_philo *philo)
+{
+	pthread_mutex_destroy(philo->central->eat);
+	pthread_mutex_destroy(philo->central->write);
+	pthread_mutex_destroy(philo->central->status);
+	pthread_mutex_destroy(philo->central->full);
+	free(philo->central->eat);
+	free(philo->central->write);
+	free(philo->central->status);
+	free(philo->central->forks);
+	free(philo->central->full);
+}
+
+void	unlock_and_destroy(t_philo *philo, t_central *central)
+{
+	destroy_forks(philo);
+	destroy_mutex(philo);
+	free(philo);
+	free(central);
+}
